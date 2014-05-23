cc32f4f8825378002dc7d8b0166472dcc5c51432
bhaskar4n <baskar4n@gmail.com>
2014-05-23 18:58:00 +0530

    electrical lib package

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+
+setup(name='electrical',
+      version='0.1',
+      description='electrical',
+      url='http://example.com',
+      author='bhaskaran',
+      author_email='baskar4n@gmail.com',
+      license='MIT License',
+      packages=['electrical'])
+     
